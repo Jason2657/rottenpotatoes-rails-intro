f6b339ff3796cecbb581ba62250514efe59ad5e1
jax2001 Jason Xie <jax2001@columbia.edu>
2025-10-04 05:28:10 +0000

    Add movie filtering by rating

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,13 @@
 class Movie < ActiveRecord::Base
+  def self.all_ratings
+    ['G', 'PG', 'PG-13', 'R']
+  end
+
+  def self.with_ratings(ratings_list)
+    if ratings_list.present?
+      where('LOWER(rating) IN (?)', ratings_list.map(&:downcase))
+    else
+      all
+    end
+  end
 end
